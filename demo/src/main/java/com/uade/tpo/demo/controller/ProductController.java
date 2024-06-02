202797d9372460b558ad290d513aed8c34442ed2
Francisco Zanon <panchizanon@gmail.com>
2024-06-02 19:16:32 -0300

    product and usr

--- demo/src/main/java/com/uade/tpo/demo/controller/ProductController.java
+++ demo/src/main/java/com/uade/tpo/demo/controller/ProductController.java
@@ -1,12 +1,33 @@
 package com.uade.tpo.demo.controller;
 
 import com.uade.tpo.demo.entity.Product;
+import com.uade.tpo.demo.entity.dto.ProductRequest;
+// import com.uade.tpo.demo.entity.dto.ProductRequest;
 import com.uade.tpo.demo.service.ProductService;
+import com.uade.tpo.demo.exceptions.ProductDuplicateException;
+import com.uade.tpo.demo.exceptions.ProductNotFoundException;
+// import exceptions.ProductDuplicateException;
+// import exceptions.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.nio.file.Path;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -18,5 +39,58 @@ public class ProductController {
     @GetMapping()
     public List<Product> buscarTodosProducts(){
         return productService.buscarTodosProducts();
+    }
+    @PostMapping
+    public ResponseEntity<Object> createProduct(@ModelAttribute ProductRequest productRequest) throws ProductDuplicateException, IOException {
+        Product result = productService.createProduct(productRequest);
+        return ResponseEntity.created(URI.create("/products/" + result.getId())).body(result);
+    }
+    @GetMapping("/{id}/image")
+    public ResponseEntity<Resource> getProductImage(@PathVariable Long id) throws IOException {
+        Product product = productService.getProductById(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        String imageUrl = product.getImageUrl();
+        if (imageUrl == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Path filePath = Paths.get("C:/Users/Panchi/Desktop/APIS-Grupo-4-Backend/demo/product-images").resolve(imageUrl.substring(imageUrl.lastIndexOf("/") + 1));
+
+        Resource resource = new UrlResource(filePath.toUri());
+        if (!resource.exists() || !resource.isReadable()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        String contentType = Files.probeContentType(filePath);
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+    @PutMapping("/{productId}")
+    public ResponseEntity<Object> updateProductAttribute(@PathVariable Long productId, @RequestBody ProductRequest updatedAttributes) {
+        try {
+            Product updatedProduct = productService.updateProductAttribute(productId, updatedAttributes);
+            return ResponseEntity.ok(updatedProduct);
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long productId) {
+        try {
+            productService.deleteProduct(productId);
+            return ResponseEntity.ok().build();
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
---
+++ demo/src/main/java/com/uade/tpo/demo/entity/dto/ProductRequest.java
@@ -0,0 +1,14 @@
+package com.uade.tpo.demo.entity.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+@Data
+public class ProductRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private float price;
+    private MultipartFile imageUrl;
+    private String category;
+    private int stock;
+}
